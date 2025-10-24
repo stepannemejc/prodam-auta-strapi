08e4f16c8cd7c8425f9bbe5f7420d639683f8599
stepannemejc <stepannemejc@users.noreply.github.com>
2025-10-24 16:44:57 +0200

    changes

--- src/api/car/routes/car.js
+++ src/api/car/routes/car.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * author router.
+ * car router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::author.author');
+module.exports = createCoreRouter('api::car.car');
